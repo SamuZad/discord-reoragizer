ac92b09713dd49eabae6721af41787918fb4a53a
samuzad <zadori.samu@gmail.com>
2021-04-04 18:07:16 +0200

    add rudimentary documentation

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='$', intents=intents, description="This is a bot for discord administrative help")
 
-@bot.command()
+@bot.command(brief='Basic ping command', description='The ping command to help make sure that the bot is alive')
 @commands.has_permissions(administrator=True)
 async def ping(ctx):
     await ctx.send('pong')
 
-@bot.command()
+@bot.command(brief='Adds members of named roles to another role', description='Example command: $add_role <new_role_name> TO <old_role_1> AND <old_role_2>. Any number of old roles can be given, but at least 1 must be given.')
 @commands.has_permissions(administrator=True)
 async def add_role(ctx, *, arg):
     new_tag = arg.split("TO")[0].strip()
